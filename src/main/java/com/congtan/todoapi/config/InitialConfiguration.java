6f4bd319c649a9bc5a59e05dc2edd6451e137f90
congtan97 <congtan98@gmail.com>
2023-02-14 14:51:48 +0700

    Add SQLDialect config

--- src/main/java/com/congtan/todoapi/config/InitialConfiguration.java
+++ src/main/java/com/congtan/todoapi/config/InitialConfiguration.java
@@ -1,5 +1,7 @@
 package com.congtan.todoapi.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import org.jooq.SQLDialect;
 import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultDSLContext;
@@ -7,12 +9,16 @@ import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
 import javax.sql.DataSource;
 
 @Configuration
-public class InitialConfiguration {
+public class InitialConfiguration extends HikariConfig {
+    @Autowired
+    private Environment env;
+
     @Autowired
     private DataSource dataSource;
 
@@ -26,11 +32,18 @@ public class InitialConfiguration {
         return new DefaultDSLContext(configuration());
     }
 
+    @Bean
     public DefaultConfiguration configuration() {
         DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
 
         jooqConfiguration.set(connectionProvider());
-        jooqConfiguration.set(new DefaultExecuteListenerProvider(new ExceptionTranslator()));
+        jooqConfiguration.set(new DefaultExecuteListenerProvider(
+                new ExceptionTranslator()
+        ));
+
+        String sqlDialectName = env.getRequiredProperty("jooq.sql.dialect");
+        SQLDialect dialect = SQLDialect.valueOf(sqlDialectName);
+        jooqConfiguration.set(dialect);
 
         return jooqConfiguration;
     }
