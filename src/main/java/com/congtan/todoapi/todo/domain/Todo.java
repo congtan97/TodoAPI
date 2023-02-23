a09fecf587821d21b167337d2e4cf354e058ee8f
congtan97 <congtan98@gmail.com>
2023-02-23 15:36:34 +0700

    Refactor domain

--- src/main/java/com/congtan/todoapi/todo/domain/Todo.java
+++ src/main/java/com/congtan/todoapi/todo/domain/Todo.java
@@ -1,2 +1,42 @@
-package com.congtan.todoapi.todo.domain;public class Todo {
+package com.congtan.todoapi.todo.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor
+@Builder
+public class Todo {
+    private Integer id;
+    private String title;
+    private String content;
+    private Boolean status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime dueDate;
+    private Integer userId;
+
+    public static Todo toCreate(String title, String content, Boolean status, LocalDateTime dueDate, Integer userId) {
+        return Todo.builder()
+                .title(title)
+                .content(content)
+                .status(status)
+                .createdAt(LocalDateTime.now(ZoneId.of("UTC")))
+                .dueDate(dueDate)
+                .userId(userId)
+                .build();
+    }
+
+    public static Todo toUpdate(String title, String content, Boolean status, LocalDateTime dueDate) {
+        return Todo.builder()
+                .title(title)
+                .content(content)
+                .status(status)
+                .updatedAt(LocalDateTime.now(ZoneId.of("UTC")))
+                .dueDate(dueDate)
+                .build();
+    }
 }
